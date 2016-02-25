8b03594e1bcd5f38a7d3b31d6629d1f2c50a73f5
Ziyuan Liu <lutepluto@gmail.com>
2016-02-25 17:43:09 +0800

    feat(navbar): add navbar styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,52 @@
 'use strict';
 
 var gulp = require('gulp'),
-  plugins = require('gulp-load-plugins')(),
-  del = require('del')
+  del = require('del'),
+  connect = require('gulp-connect'),
+  sass = require('gulp-ruby-sass')
 
 var path = require('path'),
   ROOT = path.resolve(__dirname)
 
-gulp.task('dev', function() {
-  plugins.connect.server({
+/**
+ * -------------------- Connect task --------------------
+ */
+gulp.task('connect', function() {
+  connect.server({
     root: ROOT,
     port: 8888,
-    livereload: true  
-  })  
+    livereload: true
+  })
 })
+
+/**
+ * -------------------- Image task --------------------
+ */
+gulp.task('image', function() {
+  return gulp.src('src/images/*')
+    .pipe(gulp.dest('dist/images'))
+})
+
+/**
+ * -------------------- Scss task --------------------
+ */
+gulp.task('sass', function() {
+  return sass('src/scss/jarvis.scss', {
+      style: 'expanded'
+    })
+    .on('error', sass.logError)
+    .pipe(gulp.dest('dist/css/'))
+})
+
+/**
+ * -------------------- Watch task --------------------
+ */
+gulp.task('watch', function() {
+  gulp.watch('src/scss/*.scss', ['sass'])
+  gulp.watch('src/images/*', ['image'])
+})
+
+/**
+ * -------------------- Dev task --------------------
+ */
+gulp.task('dev', ['connect', 'watch'])

7e01eb084ccb80d092eeb9a5122dec3a3db79e16
Ziyuan Liu <lutepluto@gmail.com>
2016-03-28 15:25:45 +0800

    feat(modal-adapter.js): add modal adapter script

---
+++ src/scripts/modal-adapter.js
@@ -0,0 +1,22 @@
+// Modal adpater for AJAX loading modal
+
+$(document).on('click', '[data-toggle="modal"]', (e) => {
+  e.preventDefault()
+  let $this = $(e.currentTarget),
+    url = $this.attr('href'),
+    id = $this.attr('data-target').slice(1)
+
+  if (url.indexOf('#') === 0) {
+    $(url).modal('open')
+  } else {
+    let params = $this.attr('data-form-params')
+    let data = params ? $(`#${params}`).serialize() : {}
+
+    $.get(url, data, (data) => {
+      $(`<div class="modal fade" id="${id}" role="dialog" aria-hidden="true">${data}</div>`)
+        .modal()
+        .on('hidden.bs.modal', e => $(e.currentTarget).remove())
+        .on('shown.bs.modal', e => $this.trigger('shown.modal'))
+    })
+  }
+}).off('click.bs.modal.data-api')
